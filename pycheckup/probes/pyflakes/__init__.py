8185af7f2017aacce5e02a5d750baeffc265b4b9
Michael Peterson <peterson514@gmail.com>
2012-08-03 16:08:36 -0400

    Pyflakes probe

---
+++ pycheckup/probes/pyflakes/__init__.py
@@ -0,0 +1,56 @@
+import subprocess
+from collections import defaultdict
+from pycheckup import mapreduce
+from pycheckup.document import weekly_data
+
+
+PYFLAKES_ERRORS = {
+    'UnusedImport': 'imported but unused',
+    'RedefinedWhileUnused': 'redefinition of unused',
+    'ImportShadowedByLoopVar': 'shadowed by loop variable',
+    'ImportStarUsed': 'unable to detect undefined names',
+    'UndefinedName': 'undefined name ',
+    'UndefinedExport': 'in __all__',
+    'UndefinedLocal': 'referenced before assignment',
+    'DuplicateArgument': 'duplicate argument',
+    'Redefined': 'redefinition of',
+    'LateFutureImport': 'future import(s)',
+    'UnusedVariable': 'is assigned to but never used'
+}
+
+
+def run(repo, doc, on_date):
+    cmd = ['pyflakes', repo.working_dir]
+
+    # Pyflakes violations show up in stderr.
+    # If there are none, return a total of 0.
+    try:
+        subprocess.check_output(cmd)
+        result = {'total': 0}
+    except subprocess.CalledProcessError, e:
+        result = parse_results(e.output)
+
+    doc['pyflakes'].append(weekly_data(on_date, result))
+
+
+def parse_results(results):
+    violations = defaultdict(int)
+
+    for line in results.split("\n"):
+        msg = ' '.join(line.split()[1:])
+
+        for error_name, error_search in PYFLAKES_ERRORS.iteritems():
+            if error_search in msg:
+                violations[error_name] += 1
+                break
+
+    violations['total'] = sum(violations.itervalues())
+    return violations
+
+
+def map_reduce():
+    mongo.db().repositories.map_reduce(
+        load_js('pyflakes/map.js'),
+        load_js('pyflakes/reduce.js'),
+        'summary-pyflakes'
+    )
