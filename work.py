4638a6324694eea79b6b8117c010470be2d1ec4b
Michael Peterson <peterson514@gmail.com>
2012-08-05 15:15:14 -0400

    Data for bootstrap run 2

--- work.py
+++ work.py
@@ -1,18 +1,19 @@
 import csv
-# from tasks import bootstrap_repo
-from pycheckup import mongo
+from tasks import bootstrap_repo
+# from pycheckup import mongo
 
 
-collection = mongo.db().repositories
+# collection = mongo.db().repositories
 
 
-with open('data/repos.csv') as f:
+with open('data/run2.csv') as f:
     reader = csv.reader(f)
 
-    for row in reader:user, repo = row
-        r = collection.find_one({'_id': '%s/%s' % (user, repo)})
-        if r is None:
-            print '%s,%s' % (user, repo)
-        # bootstrap_repo.delay(user, repo)
+    for row in reader:
+        user, repo = row
+        # r = collection.find_one({'_id': '%s/%s' % (user, repo)})
+        # if r is None:
+        #     print '%s,%s' % (user, repo)
+        bootstrap_repo.delay(user, repo)
 
 print 'Finished'
