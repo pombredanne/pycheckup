984c66cb03bfc17e3e3be49661235f85fb687099
Michael Peterson <peterson514@gmail.com>
2012-08-05 20:30:26 -0400

    Pending script returns # of repos left to do

--- pending_repos.py
+++ pending_repos.py
@@ -4,7 +4,10 @@ from pycheckup import mongo
 collection = mongo.db().repositories
 
 
+num = 0
 for r in collection.find({'swearing': []}).sort('popularity.data.watchers', -1):
     print r['_id'].replace('/', ',')
+    num += 1
 
-print 'Finished'
+
+print 'Finished: %s pending.' % num
