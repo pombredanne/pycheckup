b48a3b962c533731f7d4302d6c3039a8f42f4f02
Michael Peterson <peterson514@gmail.com>
2012-08-07 20:14:42 -0400

    Summary API views

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,25 @@
 from django.conf.urls.defaults import patterns
 
 
-urlpatterns = patterns('',
-    (r'^$', 'app.views.app.index'),
+urlpatterns = patterns('app.views.api',
+    (r'^api/commits/count$', 'commits_count'),
+    (r'^api/commits/lines$', 'commits_lines'),
+    (r'^api/license$', 'license'),
+    (r'^api/line-count$', 'line_count'),
+    (r'^api/line-count/python$', 'line_count_python'),
+    (r'^api/pep8$', 'pep8'),
+    (r'^api/popularity/collaborators$', 'popularity_collaborators'),
+    (r'^api/popularity/forks$', 'popularity_forks'),
+    (r'^api/popularity/issues$', 'popularity_issues'),
+    (r'^api/popularity/watchers$', 'popularity_watchers'),
+    (r'^api/pyflakes$', 'pyflakes'),
+    (r'^api/readme$', 'readme'),
+    (r'^api/setup.py$', 'setup_py'),
+    (r'^api/swearing$', 'swearing'),
+    (r'^api/tabs-or-spaces$', 'tabs_spaces'),
+)
+
+
+urlpatterns += patterns('app.views.app',
+    (r'^$', 'index'),
 )
---
+++ app/views/api.py
@@ -0,0 +1,89 @@
+import json
+from django.http import HttpResponse
+from pycheckup import mongo
+
+
+db = mongo.db()
+
+
+def json_encoder(obj):
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        raise TypeError, 'Could not encode %s' % type(obj)
+
+
+def json_response(d):
+    return HttpResponse(
+        json.dumps(d, default=json_encoder),
+        content_type='application/json'
+    )
+
+
+def mongo_time_series(name):
+    return json_response([d for d in db['summary-%s' % name].find()])
+
+
+def mongo_categories(name):
+    result = {d['_id']: d['value'] for d in db['summary-%s' % name].find()}
+    return json_response(result)
+
+
+def commits_count(request):
+    return mongo_time_series('commits-count')
+
+
+def commits_lines(request):
+    return mongo_time_series('commits-lines')
+
+
+def license(request):
+    return mongo_categories('license')
+
+
+def line_count(request):
+    return mongo_time_series('line-count')
+
+
+def line_count_python(request):
+    return mongo_time_series('line-count-python')
+
+
+def pep8(request):
+    return mongo_time_series('pep8')
+
+
+def popularity_collaborators(request):
+    return mongo_time_series('popularity-collaborators')
+
+
+def popularity_forks(request):
+    return mongo_time_series('popularity-forks')
+
+
+def popularity_issues(request):
+    return mongo_time_series('popularity-issues')
+
+
+def popularity_watchers(request):
+    return mongo_time_series('popularity-watchers')
+
+
+def pyflakes(request):
+    return mongo_time_series('pyflakes')
+
+
+def readme(request):
+    return mongo_categories('readme')
+
+
+def setup_py(request):
+    return mongo_categories('setup_py')
+
+
+def swearing(request):
+    return mongo_time_series('swearing')
+
+
+def tabs_spaces(request):
+    return mongo_categories('tabs-or-spaces')
