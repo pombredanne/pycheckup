de07701f3ed6e7a438cec37d7dd48c12c668896c
Michael Peterson <peterson514@gmail.com>
2012-08-21 14:23:12 -0400

    Project page graphs

--- app/static/javascripts/project/project.js
+++ app/static/javascripts/project/project.js
@@ -1,6 +1,11 @@
 $(function() {
   var views = [];
   views.push(new Project.LineCount());
+  views.push(new Project.Commits());
+  views.push(new Project.LinesChanged());
+  views.push(new Project.Pyflakes());
+  views.push(new Project.Pep8());
+  views.push(new Project.Swearing());
 
   _.map(views, function(v) { v.render(); });
 });
--- app/static/javascripts/project/views.js
+++ app/static/javascripts/project/views.js
@@ -3,22 +3,72 @@ var Project = {
   LineCount: Backbone.View.extend({
     el: '#project-line-count',
 
-    graphResult: function(data) {
-      var result = {
-        featured: {
-          data: PyCheckupData.line_count,
-          xAttr: 'date',
-          yAttr: 'total'
-        }
-      };
+    render: function() {
+      var data = _.map(PyCheckupData.line_count, function(d) {
+        return {x: new Date(d.date), y: d.data.py};
+      });
 
-      var graph = new pyCheckupGraphs();
-      graph.timeSeriesGraph(result, '#project-line-count .graph-container');
-    },
+      pyCheckupGraphs().timeSeriesGraph(data, '#project-line-count .graph-container');
+    }
+  }),
+
+  Commits: Backbone.View.extend({
+    el: '#project-commits',
 
     render: function() {
-      // console.log(PyCheckupData.line_count);
-      this.graphResult();
+      var data = _.map(PyCheckupData.commits, function(d) {
+        return {x: new Date(d.date), y: d.data.count};
+      });
+
+      pyCheckupGraphs().timeSeriesGraph(data, '#project-commits .graph-container');
+    }
+  }),
+
+  LinesChanged: Backbone.View.extend({
+    el: '#project-lines-changed',
+
+    render: function() {
+      var data = _.map(PyCheckupData.commits, function(d) {
+        return {x: new Date(d.date), y: d.data.lines_changed};
+      });
+
+      pyCheckupGraphs().timeSeriesGraph(data, '#project-lines-changed .graph-container');
+    }
+  }),
+
+  Pyflakes: Backbone.View.extend({
+    el: '#project-pyflakes',
+
+    render: function() {
+      var data = _.map(PyCheckupData.pyflakes, function(d) {
+        return {x: new Date(d.date), y: d.data.total};
+      });
+
+      pyCheckupGraphs().timeSeriesGraph(data, '#project-pyflakes .graph-container');
+    }
+  }),
+
+  Pep8: Backbone.View.extend({
+    el: '#project-pep8',
+
+    render: function() {
+      var data = _.map(PyCheckupData.pep8, function(d) {
+        return {x: new Date(d.date), y: d.data.total};
+      });
+
+      pyCheckupGraphs().timeSeriesGraph(data, '#project-pep8 .graph-container');
+    }
+  }),
+
+  Swearing: Backbone.View.extend({
+    el: '#project-swearing',
+
+    render: function() {
+      var data = _.map(PyCheckupData.swearing, function(d) {
+        return {x: new Date(d.date), y: d.data.total};
+      });
+
+      pyCheckupGraphs().timeSeriesGraph(data, '#project-swearing .graph-container');
     }
   })
 
